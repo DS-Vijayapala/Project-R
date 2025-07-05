2bee24086ed190ee5b16612d0a3abb108305d9c7
Dineth <dinethfiverr23@gmail.com>
2025-07-05 20:37:24 +0530

    Project Setup Done

---
+++ client/src/assets/assets.js
@@ -0,0 +1,28 @@
+import logo from './logo.png';
+import search_icon from './search_icon.png';
+
+
+
+export const assets = {
+
+    logo,
+    search_icon,
+
+}
+
+export const links = [
+
+    { name: 'Home', path: '/' },
+    { name: 'Products', path: '/products' },
+    { name: 'My Bookings', path: '/my-bookings' },
+
+];
+
+export const ownerLinks = [
+
+    { name: 'Dashboard', path: '/owner' },
+    { name: 'Add Product', path: '/owner/add-product' },
+    { name: 'Manage Products', path: '/owner/manage-products' },
+    { name: 'Manage Bookings', path: '/owner/manage-bookings' },
+
+];
