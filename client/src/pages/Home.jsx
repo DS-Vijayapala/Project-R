5ebb9068d548dabab6a5efc4bd2715e18cb7a2ce
Dineth <dinethfiverr23@gmail.com>
2025-07-07 12:06:12 +0530

    Create Banner Component And Mount It

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Featured from '../components/Featured'
+import Banner from '../components/Banner'
 
 const Home = () => {
 
@@ -11,6 +12,8 @@ const Home = () => {
             <Hero />
 
             <Featured />
+
+            <Banner />
 
         </>
 
