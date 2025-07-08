b18c624e33743d04da56d498598a54c872ba968d
Dineth <dinethfiverr23@gmail.com>
2025-07-08 16:05:37 +0530

    Mount NavBar And SideBar To Layout

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import NavBarOwner from '../../components/owner/NavBarOwner'
+import SideBar from '../../components/owner/SideBar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
 
     return (
 
-        <div>Layout</div>
+        <div className='flex flex-col'>
+
+            <NavBarOwner />
+
+            <div className='flex'>
+
+                <SideBar />
+
+                <Outlet />
+
+            </div>
+
+
+        </div>
 
     )
 
