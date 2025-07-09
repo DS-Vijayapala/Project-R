2dc372d27d28cb4f89b1e151dac5c5a14aaf032a
Dineth <dinethfiverr23@gmail.com>
2025-07-09 12:12:37 +0530

    Update User Data (Name Only)

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -106,7 +106,7 @@ export const ownerMenuLinks = [
 
 export const dummyUserData = {
     "_id": "6847f7cab3d8daecdb517095",
-    "name": "GreatStack",
+    "name": "Nathalia",
     "email": "admin@example.com",
     "role": "owner",
     "image": testimonial_image_1,
