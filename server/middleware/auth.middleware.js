486eb3052f9969ac16afe451ea8dc17fce1af4d7
Dineth <dinethfiverr23@gmail.com>
2025-07-15 14:33:10 +0530

    Create Auth Middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,49 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.models.js";
+
+export const protect = async (req, res, next) => {
+
+    const token = req.headers.authorization;
+
+    if (!token) {
+
+        return res.json({
+
+            success: false,
+            message: "Access denied. No token provided.",
+
+        });
+
+    }
+
+    try {
+
+        const userId = jwt.decode(token, process.env.JWT_SECRET);
+
+        if (!userId) {
+
+            return res.json({
+                success: false,
+                message: "Invalid token payload. User ID missing.",
+
+            });
+
+        }
+
+        req.user = await User.findById(userId).select("-password");
+
+        next();
+
+    } catch (error) {
+
+        console.log(error.message);
+
+        return res.json({
+            success: false,
+            message: "Invalid or expired token.",
+
+        });
+
+    }
+
+};
