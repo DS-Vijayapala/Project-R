b9a4c3c1091add9cb4cdd5ab56fe3162e277efc8
Dineth <dinethfiverr23@gmail.com>
2025-07-15 15:00:27 +0530

    Create Owner Controller And Change Role To Owner

---
+++ server/controllers/owner.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/user.models.js"
+
+
+
+
+
+// Change Role To Owner
+
+export const changeRoleToOwner = async (req, res) => {
+
+    try {
+
+        const { _id } = req.user;
+
+        await User.findByIdAndUpdate(_id, { role: "owner" });
+
+        res.json({
+
+            success: true,
+            message: "You can now list your cars.",
+
+        });
+
+    } catch (error) {
+
+        console.log(error.message);
+        res.json({
+
+            success: false,
+            message: "Failed to update role.",
+
+        });
+
+    }
+
+};
