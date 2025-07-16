aa3816d2ea458f6051d6fce8b3ad8b09fccee1e0
Dineth <dinethfiverr23@gmail.com>
2025-07-16 14:38:26 +0530

    Create Route GET Owner Product List

--- server/routes/owner.routes.js
+++ server/routes/owner.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { protect } from '../middleware/auth.middleware.js';
-import { changeRoleToOwner, addNewProduct } from '../controllers/owner.controller.js';
+import { changeRoleToOwner, addNewProduct, getOwnerProducts } from '../controllers/owner.controller.js';
 import Upload from '../middleware/multer.middleware.js'
 
 
@@ -14,7 +14,8 @@ ownerRouter.post('/change-role', protect, changeRoleToOwner)
 
 ownerRouter.post('/add-product', Upload.single("image"), protect, addNewProduct)
 
+// GET Owner Product List
 
-
+ownerRouter.get('/get-products-list', protect, getOwnerProducts)
 
 export default ownerRouter
