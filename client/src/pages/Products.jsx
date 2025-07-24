ea30d9ca525a178c91bd308908644381a644d183
Dineth <dinethfiverr23@gmail.com>
2025-07-24 15:37:23 +0530

    Add Filteration Logics In Products Page

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,12 +1,116 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../components/Title'
-import { assets, dummyCarData } from '../assets/assets'
+import { assets } from '../assets/assets'
 import ProductCard from '../components/ProductCard'
-
+import { useSearchParams } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Products = () => {
 
+    // Getting Search Params From URL
+
+    const [searchParams] = useSearchParams()
+
+    const pickupLocation = searchParams.get('pickupLocation')
+
+    const pickupDate = searchParams.get('pickupDate')
+
+    const returnDate = searchParams.get('returnDate')
+
+    const { products, axios } = useAppContext()
+
     const [input, setInput] = useState('')
+
+    const [filteredProducts, setFilteredProducts] = useState([])
+
+    const isSearchData = pickupLocation && pickupDate && returnDate
+
+    // Apply filter based on input search text
+
+    const applyFilter = () => {
+
+        if (input === '') {
+
+            setFilteredProducts(products)
+
+        } else {
+
+            const lowerInput = input.toLowerCase()
+
+            const filtered = products.filter((p) =>
+
+                p.brand.toLowerCase().includes(lowerInput) ||
+                p.location.toLowerCase().includes(lowerInput)
+
+            )
+
+            setFilteredProducts(filtered)
+
+        }
+
+    }
+
+    // Want to Fix Bug
+
+    const searchProductAvailability = async () => {
+
+        try {
+
+            const { data } = await axios.post('/api/booking-data/check-availability', {
+                location: pickupLocation,
+                pickupDate,
+                returnDate
+
+            })
+
+            if (data.success) {
+
+                setFilteredProducts(data.availableProducts)
+
+                console.log(data.availableProducts);
+
+
+                if (data.availableProducts.length === 0) {
+
+                    toast('No Product Available')
+
+                }
+
+            } else {
+
+                toast.error('Failed to fetch availability')
+
+            }
+
+        } catch (error) {
+
+            toast.error('Error checking availability')
+
+        }
+
+    }
+
+    useEffect(() => {
+
+        if (isSearchData) {
+
+            searchProductAvailability()
+
+        }
+
+    }, [pickupLocation, pickupDate, returnDate])
+
+    useEffect(() => {
+
+        if (!isSearchData && products.length > 0) {
+
+            applyFilter()
+
+        }
+
+    }, [input, products, isSearchData])
+
 
     return (
 
@@ -20,26 +124,29 @@ const Products = () => {
 
                 {/* Search and Filter Bar */}
 
-                <div className='flex items-center bg-white px-4 mt-6 max-w-140 w-full 
-                h-12 rounded-full shadow-md'>
+                <div className='flex items-center bg-white px-4 mt-6 max-w-140 
+                w-full h-12 rounded-full shadow-md'>
 
                     <img
                         src={assets.search_icon}
                         alt="search_icon"
-                        className='w-5 h-5 mr-3 opacity-75' />
+                        className='w-5 h-5 mr-3 opacity-75'
+                    />
 
                     <input
                         onChange={(e) => setInput(e.target.value)}
                         value={input}
                         type="text"
-                        placeholder='Search for cars, motorcycles, and more...'
-                        className='w-full h-full text-gray-700 placeholder-gray-400 
-                        rounded-full  outline-none px-4' />
+                        placeholder="Search by Brand or Nearest Location..."
+                        className='w-full h-full text-gray-700 placeholder-gray-400
+                         rounded-full outline-none px-4'
+                    />
 
                     <img
                         src={assets.filter_icon}
                         alt="filter_icon"
-                        className='w-5 h-5 ml-3 opacity-75 cursor-pointer' />
+                        className='w-5 h-5 ml-3 opacity-75 cursor-pointer'
+                    />
 
                 </div>
 
@@ -49,25 +156,28 @@ const Products = () => {
 
             <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-10">
 
-                <p className="text-gray-600 xl:px-20 max-w-7xl mx-auto mb-5 ">Showing {dummyCarData.length} Products</p>
+                <p className="text-gray-600 xl:px-20 max-w-7xl mx-auto mb-5">
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 
-                xl:px-20 max-w-7xl mx-auto">
+                    Showing {filteredProducts.length} Products
 
-                    {dummyCarData.map((product, index) => {
+                </p>
 
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
+                gap-8 mt-4 xl:px-20 max-w-7xl mx-auto">
 
-                        return (
-                            <div key={index} className="bg-white p-4 rounded-lg shadow-md 
-                            hover:shadow-lg transition-shadow duration-300">
+                    {filteredProducts.map((product, index) => (
 
-                                <ProductCard product={product} />
+                        <div
+                            key={index}
+                            className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg 
+                            transition-shadow duration-300"
+                        >
 
-                            </div>
+                            <ProductCard product={product} />
 
-                        );
+                        </div>
 
-                    })}
+                    ))}
 
                 </div>
 
