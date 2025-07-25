653a047780838e2ae7349b263e402c128a57da65
Dineth <dinethfiverr23@gmail.com>
2025-07-25 14:42:03 +0530

    Animate Hero Section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { assets, cityList } from '../assets/assets'
 import { useAppContext } from '../context/AppContext'
+import { motion } from 'motion/react'
 
 const Hero = () => {
 
@@ -27,19 +28,30 @@ const Hero = () => {
 
     return (
 
-        <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
 
-            <h1
+            <motion.h1
+                initial={{ y: 50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.2 }}
                 className="text-4xl md:text-5xl font-bold leading-snug tracking-tight 
              bg-gradient-to-r from-pink-500 via-yellow-500 to-indigo-500 
              text-transparent bg-clip-text text-center"
             >
-                Rent Anything. <br className="hidden md:block" />
+                Rent Your Car. <br className="hidden md:block" />
                 Anytime. Anywhere.
-            </h1>
+            </motion.h1>
 
-            <form onSubmit={handleSearch} className='flex flex-col md:flex-row items-start md:items-center justify-between
-            p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-180 bg-white shadow-[0_8px_20px_rgba(0,0,0,0.1)]'>
+            <motion.form
+                initial={{ scale: 0.95, opacity: 0, y: 50 }}
+                animate={{ scale: 1, y: 0, opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.4 }}
+                onSubmit={handleSearch} className='flex flex-col md:flex-row items-start md:items-center justify-between
+                p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-180 bg-white shadow-[0_8px_20px_rgba(0,0,0,0.1)]'>
 
                 <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
 
@@ -96,7 +108,9 @@ const Hero = () => {
 
                 </div>
 
-                <button
+                <motion.button
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
                     type="submit"
                     className="flex items-center justify-center gap-2 px-6 py-3 max-sm:mt-4
                                      bg-green-600 hover:bg-green-700 text-white font-medium 
@@ -111,15 +125,19 @@ const Hero = () => {
 
                     Search
 
-                </button>
+                </motion.button>
 
 
 
-            </form>
+            </motion.form>
 
-            <img src={assets.main_car} alt="car" className='max-h-74' />
+            <motion.img
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.6 }}
+                src={assets.main_car} alt="car" className='max-h-74' />
 
-        </div>
+        </motion.div>
 
     )
 }
